dac2f41fb6f695a87ddf2bbf1ee895513f790693
Wessley Alberts <wessley.alberts@npbhs.school.nz>
2022-08-17 14:27:16 +1200

    First Redesign

--- code/menu.py
+++ code/menu.py
@@ -8,8 +8,10 @@ def test():
         
 """Creates the layout of the Main Menu"""
 #Sets background colour to a variable
-bg_colour = "#a8a8a8"
-toolbar_colour = "#111fff"
+bg_colour = "#858585"
+button_colour = "#707070"
+toolbar_colour = "#5c5c5c"
+text_colour = "#f5f5f5"
 
 #Sets the main menu window
 master = Tk()
@@ -22,25 +24,25 @@ master.title("Main Menu")
 pixel = PhotoImage(width = 1, height = 1)
 
 
-toolbar = Frame(master, bg = toolbar_colour, width = 1600, height = 100)
+toolbar = Frame(master, bg = toolbar_colour, width = 1600, height = 200)
 toolbar.place(x = 0, y = 0)
 
-game_title = Label(toolbar, bg = toolbar_colour, text = "17 character name", font = ("TkDefaultFont", 36), image = pixel, width = 400, height = 50, compound = "c")
-game_title.place(x = 600, y = 10)
+game_title = Label(toolbar, bg = toolbar_colour, fg = text_colour, text = "17 character name", font = ("TkDefaultFont", 36), image = pixel, width = 600, height = 150, compound = "c")
+game_title.place(x = 500, y = 20)
 
-close_button = Button(toolbar, bg = toolbar_colour, text = "Close", font = ("TkDefaultFont", 24), command = close_window, image = pixel, height = 50, width = 50, compound = "c")
-close_button.place(x = 1500, y = 10)
+close_button = Button(toolbar, bg = toolbar_colour, fg = text_colour, text = "Close", font = ("TkDefaultFont", 24), command = close_window, image = pixel, height = 160, width = 160, compound = "c")
+close_button.place(x = 1420, y = 20)
 
-body = Frame(master, bg = bg_colour, width = 1600, height = 800)
-body.place(x = 0, y = 100)
+body = Frame(master, bg = bg_colour, width = 1600, height = 700)
+body.place(x = 0, y = 200)
 
-test_button1 = Button(body, bg = bg_colour, text = "Test", font = ("TkDefaultFont", 36), command = "Test", image = pixel, height = 150, width = 150, compound = "c")
-test_button1.place(x = 100, y = 50)
-test_button2 = Button(body, bg = bg_colour, text = "Test", font = ("TkDefaultFont", 36), command = "Test", image = pixel, height = 150, width = 150, compound = "c")
-test_button2.place(x = 100, y = 500)
-test_button3 = Button(body, bg = bg_colour, text = "Test", font = ("TkDefaultFont", 36), command = "Test", image = pixel, height = 150, width = 150, compound = "c")
-test_button3.place(x = 1000, y = 50)
-test_button4 = Button(body, bg = bg_colour, text = "Test", font = ("TkDefaultFont", 36), command = "Test", image = pixel, height = 150, width = 150, compound = "c")
-test_button4.place(x = 1000, y = 500)
+test_button1 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button1.place(x = 200, y = 100)
+test_button2 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button2.place(x = 200, y = 380)
+test_button3 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button3.place(x = 850, y = 100)
+test_button4 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button4.place(x = 850, y = 380)
 
 master.mainloop()
