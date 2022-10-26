b0a53774b268a47bb377b63149163224d9907c03
Wessley Alberts <wessley.alberts@npbhs.school.nz>
2022-10-27 11:33:32 +1300

    Made menu buttons work

--- code/menu.py
+++ code/menu.py
@@ -1,11 +1,9 @@
 from tkinter import *
+import os
 
 def close_window():
     """Closes the window when called"""
     master.destroy()
-
-def test():
-    return "test"
         
 """Creates the Main Menu"""
 #Sets background colour to a variable
@@ -28,7 +26,7 @@ toolbar = Frame(master, bg = toolbar_colour, width = 1600, height = 200)
 toolbar.place(x = 0, y = 0)
 
 #Creates the title for the game
-game_title = Label(toolbar, bg = toolbar_colour, fg = text_colour, text = "17 character name", font = ("TkDefaultFont", 36), image = pixel, width = 600, height = 150, compound = "c")
+game_title = Label(toolbar, bg = toolbar_colour, fg = text_colour, text = "Testing Staion", font = ("TkDefaultFont", 36), image = pixel, width = 600, height = 150, compound = "c")
 game_title.place(x = 500, y = 20)
 
 #Creates the close button (Needed because of -fullscreen being True)
@@ -40,11 +38,11 @@ body = Frame(master, bg = bg_colour, width = 1600, height = 700)
 body.place(x = 0, y = 200)
 
 #Creates the buttons (currently used for testing purposes)
-test_button1 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button1 = Button(body, bg = button_colour, text = "Number Memory", font = ("TkDefaultFont", 36), command = lambda : os.system('python memory.py'), image = pixel, height = 220, width = 550, compound = "c")
 test_button1.place(x = 200, y = 100)
-test_button2 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button2 = Button(body, bg = button_colour, text = "Aim Test", font = ("TkDefaultFont", 36), command = lambda : os.system('python aim.py'), image = pixel, height = 220, width = 550, compound = "c")
 test_button2.place(x = 525, y = 380)
-test_button3 = Button(body, bg = button_colour, text = "Test", font = ("TkDefaultFont", 36), command = "test", image = pixel, height = 220, width = 550, compound = "c")
+test_button3 = Button(body, bg = button_colour, text = "Word Memory", font = ("TkDefaultFont", 36), command = lambda : os.system('python memory_word.py'), image = pixel, height = 220, width = 550, compound = "c")
 test_button3.place(x = 850, y = 100)
 
 master.mainloop()
